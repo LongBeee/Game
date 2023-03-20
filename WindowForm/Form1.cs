32f08c28d4a8efb9be2316277f715a8f153751b5
LongBeee <125204560+LongBeee@users.noreply.github.com>
2023-03-20 17:16:47 +0900

    Add files via upload

--- WindowForm/Form1.cs
+++ WindowForm/Form1.cs
@@ -3,91 +3,97 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
 
-namespace Form03_14_3
+namespace Form03_20_1
 {
     public partial class Form1 : Form
     {
-        private Random random = new Random();
-        
         public Form1()
         {
             InitializeComponent();
-            drawPanel1.BackColor = Color.Orange;
-            drawPanel2.BackColor = Color.Orange;
-            drawPanel3.BackColor = Color.Orange;
         }
-
-        private void startBtn_Click(object sender, EventArgs e)
+        private void Open_Click(object sender, EventArgs e)
         {
-            Thread thread1 = new Thread(new ThreadStart(DoWork));
-            thread1.IsBackground = true;
-            thread1.Start();
+            OpenFileDialog openFile = new OpenFileDialog();
 
-            // 스레드가 종료될 때까지 대기
-            while (thread1.IsAlive)
+            openFile.Filter = "텍스트파일(*.txt)|*.txt|모든파일(*.*)|*.*";
+
+            openFile.InitialDirectory = @"C:\Users\ICI\Desktop";
+            openFile.RestoreDirectory = true;
+            openFile.ShowDialog();
+
+            //openFile(openFileDialog1.FileName);
+
+            if (openFile.FileName.Length > 0)
             {
-                Application.DoEvents(); // 이벤트 처리를 위한 대기
-            }
-        }
+                string fileContent = File.ReadAllText(openFile.FileName);
 
-        private void DoWork()
-        {
-            while (true)
-            {
-                int randomNumber1 = random.Next(0, 2); 
-                UpdatePanel(drawPanel1, randomNumber1.ToString());
+                // textBox1.Text에 파일 내용 할당하기
+                textBox1.Text = fileContent;
 
-                int randomNumber2 = random.Next(0, 2);
-                UpdatePanel(drawPanel2, randomNumber2.ToString());
-
-                int randomNumber3 = random.Next(0, 2);
-                UpdatePanel(drawPanel3, randomNumber3.ToString());
-
-                if ((randomNumber1 == randomNumber2) && (randomNumber2 == randomNumber3) && (randomNumber1 == randomNumber3))
-                {
-                    label1.Text = "축하드립니다!!";
-                    break;
-                }
-
-                Thread.Sleep(1000);
             }
         }
 
-        private void UpdatePanel(Panel panel, string text)
+        private void Save_Click(object sender, EventArgs e)
         {
-            if (panel.InvokeRequired)
+            string fileName;
+            saveFileDialog1.Title = "저장경로를 지정하세요";
+            saveFileDialog1.OverwritePrompt = true;
+            saveFileDialog1.Filter = "텍스트파일(*.txt)|*.txt|모든파일(*.*)|*.*";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                panel.Invoke(new MethodInvoker(() => UpdatePanel(panel, text)));
+                fileName = saveFileDialog1.FileName;
+                SaveFile(fileName);
             }
-            else
+        }
+
+        private void SaveFile(string filename)
+        {
+            File.WriteAllText(filename, textBox1.Text);
+        }
+        int i = 1;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = i++;
+
+            if(progressBar1.Value >= 100)
             {
-                panel.Controls.Clear();
-
-                panel.Invalidate();
-
-                panel.Paint += (sender, e) =>
-                {
-                    e.Graphics.Clear(panel.BackColor);
-
-                    using (var brush = new SolidBrush(panel.ForeColor))
-                    {
-                        var font = new Font(panel.Font.FontFamily, 20, panel.Font.Style);
-
-                        var textSize = e.Graphics.MeasureString(text, font);
-                        var x = (panel.ClientSize.Width - textSize.Width) / 2;
-                        var y = (panel.ClientSize.Height - textSize.Height) / 2;
-                        e.Graphics.DrawString(text, font, brush, x, y);
-
-                        font.Dispose();
-                    }
-                };
+                i = 0;
             }
+
+            //Console.WriteLine("i = " + i++);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+            if (e.Equals("Equals"))
+            {
+
+            }
+        }
+
+        private void RedBar_Scroll(object sender, EventArgs e)
+        {
+            DrawColor.BackColor = Color.FromArgb(RedBar.Value, 0, 0);
+        }
+
+        private void GreenBar_Scroll(object sender, EventArgs e)
+        {
+            DrawColor.BackColor = Color.FromArgb(0, GreenBar.Value, 0);
+        }
+
+        private void BlueBar_Scroll(object sender, EventArgs e)
+        {
+            DrawColor.BackColor = Color.FromArgb(0, 0, BlueBar.Value);
         }
     }
 }
